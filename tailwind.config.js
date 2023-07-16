9c0ac29457b8199a9ee19310c261c05840c4110b
Leon-web-net <letiobi66@gmail.com>
2023-07-16 15:33:33 +0100

    Day1-changes

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,tsx,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
