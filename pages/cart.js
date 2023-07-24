f775c565da2f1466230cf423321d04474b6eea9e
Leon-web-net <letiobi66@gmail.com>
2023-07-24 14:06:43 +0100

    Day7-changes

--- pages/cart.js
+++ pages/cart.js
@@ -5,8 +5,9 @@ import React, { useContext } from "react";
 import Image from "next/image";
 import { XCircleIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
 
-export default function CartScreen() {
+function CartScreen() {
   const router = useRouter();
 
   const { state, dispatch } = useContext(Store);
@@ -112,3 +113,5 @@ export default function CartScreen() {
     </>
   );
 }
+
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
