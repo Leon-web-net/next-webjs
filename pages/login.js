28da5ad273856cb62d4bdabd5b3d6eca871292af
Leon-web-net <letiobi66@gmail.com>
2023-07-26 22:53:57 +0100

    Day8-changes

---
+++ pages/login.js
@@ -0,0 +1,77 @@
+import Layout from "@/components/Layout";
+import React from "react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+export default function LoginScreen() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const submitHandler = ({ email, password }) => {
+    console.log(email, password);
+  };
+
+  return (
+    <>
+      <Layout title="Login">
+        <form
+          className="mx-auto max-w-screen-md"
+          onSubmit={handleSubmit(submitHandler)}
+        >
+          <h1 className="mb-4 text-xl">Login</h1>
+          <div className="mb-4">
+            {" "}
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              {...register("email", {
+                required: "Please enter email",
+                pattern: {
+                  value:
+                    /^[a-zA-z0-9_.+-]+@[a-zA-z0-9_.+-]+.[a-zA-z0-9_.+-]+$/i,
+                  message: "Please enter valid email",
+                },
+              })}
+              className="w-full"
+              id="email"
+              autoFocus
+            ></input>
+            {errors.email && (
+              <div className="text-red-500">{errors.email.message}</div>
+            )}
+          </div>
+          <div className="mb-4">
+            {" "}
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              {...register("password", {
+                required: "Please neter password",
+                minLength: {
+                  value: 5,
+                  message: "password is more than 5 chars",
+                },
+              })}
+              className="w-full"
+              id="password"
+              autoFocus
+            ></input>
+            {errors.password && (
+              <div className="text-red-500">{errors.password.message}</div>
+            )}
+          </div>
+          <div className="mb-4">
+            <button className="primary-button">Login</button>
+          </div>
+          <div>
+            Don&apos;t have an account? &nbsp;
+            <Link href="register">Register</Link>
+          </div>
+        </form>
+      </Layout>
+    </>
+  );
+}
